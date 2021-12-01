04ea4999e606493e4ce95872da7759e7a8ce98a0
Richard Hess <eswat2@gmail.com>
2021-12-01 07:48:46 -0800

    refactor spinner into simple component

--- src/components/proto-autos.svelte
+++ src/components/proto-autos.svelte
@@ -1,11 +1,10 @@
 <script>
   import Eswat2 from './eswat2-io.svelte';
   import Footer from './footer.svelte';
+  import Spinner from './spinner.svelte';
   import ToolBar from './toolbar.svelte';
   import DealerView from './dealer-view.svelte';
   import { state, tw } from '../utils';
-
-  const LOADING = 'loading...';
 
   const { loading } = state;
 </script>
@@ -31,31 +30,7 @@
       )}
     />
   {:else}
-    <svg
-      class="animate-spin h-24px w-24px text-clrs-navy"
-      fill="none"
-      viewBox="0 0 24 24"
-      role="img"
-      aria-label="title"
-    >
-      <title>{LOADING}</title>
-      <g>
-        <circle
-          class="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          stroke-width="4"
-        />
-        <path
-          class="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        />
-      </g>
-      <path d="M0 0h24v24H0z" fill="none" />
-    </svg>
+    <Spinner />
   {/if}
   <DealerView />
   <Footer />
---
+++ src/components/spinner.svelte
@@ -0,0 +1,29 @@
+<script>
+  const LOADING = 'loading...';
+</script>
+
+<svg
+  class="animate-spin h-24px w-24px text-clrs-navy"
+  fill="none"
+  viewBox="0 0 24 24"
+  role="img"
+  aria-label="title"
+>
+  <title>{LOADING}</title>
+  <g>
+    <circle
+      class="opacity-25"
+      cx="12"
+      cy="12"
+      r="10"
+      stroke="currentColor"
+      stroke-width="4"
+    />
+    <path
+      class="opacity-75"
+      fill="currentColor"
+      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+    />
+  </g>
+  <path d="M0 0h24v24H0z" fill="none" />
+</svg>
