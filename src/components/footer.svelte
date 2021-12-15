5e6a79b552e32f06f5544c09081948690713b1a4
Richard Hess <eswat2@gmail.com>
2021-12-15 11:46:24 -0800

    refactor tw-label

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,9 +1,7 @@
 <script>
-  const TW_VERSION = '3.0.5';
+  import TwLabel from './tw-label.svelte';
 </script>
 
 <div class="flex flex-row mt-2 mb-4 mr-2">
-  <span class="ml-auto align-top text-xs italic text-clrs-slate4">
-    Tailwind {TW_VERSION}
-  </span>
+  <TwLabel />
 </div>
---
+++ src/components/tw-label.svelte
@@ -0,0 +1,7 @@
+<script>
+  const TW_VERSION = "3.0.5"
+</script>
+
+<span class="ml-auto align-top text-xs italic text-clrs-slate4">
+  Tailwind {TW_VERSION}
+</span>
