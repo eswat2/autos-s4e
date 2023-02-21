4c7359c1fffd6428e078e4997ebb36efeb2864ff
Richard Hess <eswat2@gmail.com>
2023-02-21 10:00:37 -0800

    target blank

--- src/components/eswat2-io.svelte
+++ src/components/eswat2-io.svelte
@@ -7,6 +7,7 @@
   class="absolute top-0 right-0 mt-6 mr-6 text-clrs-gray hover:text-clrs-navy"
   href={url}
   aria-label={who}
+  target="blank"
   title={who}
 >
   <proto-ikon-loader name="fingerprint" size="24" label="eswat2" />
